1d360c00acdd284002e60ff60acc81f830b320bb
AlejandroLondonoValle <londonovalleluisalejandro@gmail.com>
2026-06-08 10:04:27 -0500

    Feat: Se crea la clase DriveFile

---
+++ Models/DriveFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StudyGo.Models
+{
+    public class DriveFile
+    {
+        public Guid Id { get; set; }
+        public Guid OwnerId { get; set; }
+        public Guid CourseId { get; set; }
+        public string DriveFileId { get; set; }
+        public string Url { get; set; }
+
+        public User Owner { get; set; }
+        public Course Course { get; set; }
+    }
+}
