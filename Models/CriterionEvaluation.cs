6d2bee8b0ffa56433d1af8cf08d7ecd767038b94
AlejandroLondonoValle <londonovalleluisalejandro@gmail.com>
2026-06-08 09:40:59 -0500

    Feat: Se crea la clase CriterionEvaluation

---
+++ Models/CriterionEvaluation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StudyGo.Models
+{
+    public class CriterionEvaluation
+    {
+        public Guid Id { get; set; }
+        public Guid GradeId { get; set; }
+        public Guid RubricCriteriaId { get; set; }
+        public decimal Score { get; set; }
+        public string Comment { get; set; }
+
+        public Grade Grade { get; set; }
+        public RubricCriteria RubricCriteria { get; set; }
+    }
+}
