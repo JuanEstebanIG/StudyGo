efb08618fef86604ef7108e0b0c4e56c908d4c44
AlejandroLondonoValle <londonovalleluisalejandro@gmail.com>
2026-06-08 09:27:40 -0500

    Feat: Se crea la clase Quiz

---
+++ Models/Quiz.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using StudyGo.Enums;
+
+namespace StudyGo.Models
+{
+    public class Quiz : Activity
+    {
+        public SelectionMode SelectionMode { get; set; }
+
+        public ICollection<QuizAttempt> QuizAttempts { get; set; } = new List<QuizAttempt>();
+    }
+}
