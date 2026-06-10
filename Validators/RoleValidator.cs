71e31d1f714c94d3fa0bd32c98ebee1f1585017b
AlejandroLondonoValle <londonovalleluisalejandro@gmail.com>
2026-06-10 06:50:31 -0500

    Feat: Se crea el validator RoleValidator

---
+++ Validators/RoleValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using StudyGo.ViewModels;
+
+namespace StudyGo.Validators
+{
+    public class RoleValidator : AbstractValidator<RoleViewModel>
+    {
+        public RoleValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("El nombre del rol es obligatorio.")
+                .MinimumLength(2).WithMessage("El nombre debe tener al menos 2 caracteres.")
+                .MaximumLength(100).WithMessage("El nombre no puede superar los 100 caracteres.");
+        }
+    }
+}
